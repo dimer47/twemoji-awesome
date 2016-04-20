df5a58541deda5a3e8f1f694a6ca6c983311527c
abr4xas <me@abr4xas.org>
2016-04-20 19:00:13 +0000

    Actualizando repo Se crea v 2.0.0 del repo. Los iconos ahora usan los nombres de las imagenes fuentes para dar mayor soporte a todos los emojis.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,8 +2,13 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     pkg = require('./package.json'),
     header = require('gulp-header'),
+    size = require('gulp-size'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    minifyCSS = require('gulp-clean-css'),
     banner = ['/**',
         ' * <%= pkg.name %> - <%= pkg.description %>',
+        ' * @author <%= pkg.author %>',
         ' * @version v<%= pkg.version %>',
         ' * @link <%= pkg.homepage %>',
         ' * @license <%= pkg.license %>',
@@ -11,11 +16,25 @@ var gulp = require('gulp'),
     ].join('\n');
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
+        .pipe(size({
+            gzip: true,
+            showFiles: true
+        }))
         .pipe(header(banner, {
             pkg: pkg
         }))
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
+        .pipe(gulp.dest('./dist/'))
+        .pipe(minifyCSS('*'))
+        .pipe(size({
+            gzip: true,
+            showFiles: true
+        }))
+        .pipe(header(banner, {
+            pkg: pkg
+        }))
+        .pipe(rename('twemoji-awesome.min.css'))
+        .pipe(gulp.dest('./dist/'));
 });
 
 //Watch task
